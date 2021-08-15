cc244ff0aec98780d736ffaafcb5edbd30a43b11
majedalswaeer <majedalswaeer69@gmail.com>
2021-08-15 16:53:54 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import Form from 'react-bootstrap/Form'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      citydata: {},
+      dataforcoun: '',
+      showList:false,
+      searchCity:'',
+    }
+  }
+
+
+  getlocation = async (e) => {
+    
+
+    e.preventDefault();
+
+    
+    await this.setState({
+      dataforcoun: e.target.cityy.value 
+    })
+    let url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.dataforcoun}&format=json`;
+
+    // send the endpoint to the third party API USING AXIOS
+    let sendData = await axios.get(url);
+
+    await this.setState({
+      citydata: sendData.data[0],
+      showList:true,
+
+    })
+  }
+
+
+  render() {
+    return(
+      <div>
+        <h1>City Explorer</h1>
+        <form onSubmit={this.getlocation}>
+          <input on type='text' name='cityy' placeholder='Type a City Name!'/>
+          <button>submit</button>
+        </form>
+        {this.state.showList &&  <ul>
+          <li>City Name:{this.state.citydata.display_name}</li>
+          <li>Lat:{this.state.citydata.lat}</li>
+          <li>Lon:{this.state.citydata.lon}</li>
+        </ul>}
+      </div>
+    )
+  }
 }
 
 export default App;
+
