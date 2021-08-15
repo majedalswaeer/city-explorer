9cb081f0a41b7ce3bd8b963be949ee6b605354db
majedalswaeer <majedalswaeer69@gmail.com>
2021-08-15 19:18:04 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import axios from 'axios';
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Image from 'react-bootstrap/Image';
+import './App.css'
 
 // import Form from 'react-bootstrap/Form'
 
@@ -9,51 +14,60 @@ class App extends React.Component {
     this.state = {
       citydata: {},
       dataforcoun: '',
-      showList:false,
-      searchCity:'',
+      showList: false,
+      searchCity: '',
+      mapUrlll: ``,
     }
   }
 
 
   getlocation = async (e) => {
-    
-
     e.preventDefault();
 
-    
     await this.setState({
-      dataforcoun: e.target.cityy.value 
+      dataforcoun: e.target.cityy.value
     })
     let url = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${this.state.dataforcoun}&format=json`;
 
-    // send the endpoint to the third party API USING AXIOS
     let sendData = await axios.get(url);
 
     await this.setState({
       citydata: sendData.data[0],
-      showList:true,
+      showList: true,
 
     })
   }
 
 
   render() {
-    return(
-      <div>
-        <h1>City Explorer</h1>
-        <form onSubmit={this.getlocation}>
-          <input on type='text' name='cityy' placeholder='Type a City Name!'/>
-          <button>submit</button>
-        </form>
-        {this.state.showList &&  <ul>
-          <li>City Name:{this.state.citydata.display_name}</li>
-          <li>Lat:{this.state.citydata.lat}</li>
-          <li>Lon:{this.state.citydata.lon}</li>
-        </ul>}
+    return (
+      <div className='cont'>
+        <>
+          <h1>City Explorer</h1>
+          <div className='form'>
+          <Form onSubmit={this.getlocation}>
+          <Form.Group className="mb-3" controlId="formtext">
+            <Form.Label>City Name</Form.Label>
+            <Form.Control name='cityy' placeholder="Type a City Name!" />
+            </Form.Group>
+            <Button type='submit'>Explore!</Button>
+          </Form>
+          </div>
+
+
+          {this.state.showList &&(<p> <ListGroup style={{paddingLeft:'50ch',display: 'flex',marginInline:'10ch',color:'red', justifyContent:'space-evenly'}} >
+            <ListGroup.Item style={{padding:'2ch'}}  variant="info">City Name:{this.state.citydata.display_name}</ListGroup.Item>
+            <ListGroup.Item style={{padding:'2ch'}} variant="info">Lat:{this.state.citydata.lat}</ListGroup.Item>
+            <ListGroup.Item style={{padding:'2ch'}} variant="info">Lon:{this.state.citydata.lon}</ListGroup.Item>
+          </ListGroup> </p>)}
+          {this.state.showList  && ( <div className='img'> <Image
+              src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&center=${this.state.citydata.lat},${this.state.citydata.lon}&zoom=18`} 
+              rounded
+            /></div>)}
+        </>
       </div>
     )
   }
 }
 
 export default App;
-
